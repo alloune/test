948f5fb29e7eb7d62a31c8a6ef6252ac5e225523
Laurent Beauvisage <laurentbeauvisage@gmail.com>
2021-04-27 17:47:13 +0200

    Update method name

--- tests/Unit/DonationFeeTest.php
+++ tests/Unit/DonationFeeTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class DonationFeeTest extends TestCase
 {
 
-    public function testCommissionAmountIs10CentsFormDonationOf100CentsAndCommissionOf10Percent()
+    public function test_commission_amount_is_10_cent_form_donation_of_100_cents_and_commission_of_10_percent()
     {
         // Etant donné une donation de 100 et commission de 10%
         $donationFees = new \App\Support\DonationFee(100, 10);
@@ -20,7 +20,7 @@ class DonationFeeTest extends TestCase
         $this->assertEquals($expected, $actual);
     }
 
-    public function testCommissionAmountIs20CentsFormDonationOf200CentsAndCommissionOf10Percent()
+    public function test_commission_amount_is_20_cents_form_donation_of_200_cents_and_commission_of_10_percent()
     {
         // Etant donné une donation de 200 et commission de 10%
         $donationFees = new \App\Support\DonationFee(200, 10);
